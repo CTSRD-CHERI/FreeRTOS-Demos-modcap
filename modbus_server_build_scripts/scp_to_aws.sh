4cda46bf28fbb8034a24603d61996db661dbde6f
Michael Dodson <md403@cam.ac.uk>
2021-05-18 13:30:59 +0100

    scripts: Parameterise scp script

--- modbus_server_build_scripts/scp_to_aws.sh
+++ modbus_server_build_scripts/scp_to_aws.sh
@@ -7,16 +7,27 @@
 set -e
 set -u
 
-BIN_DIR=./bin
 USERNAME=ubuntu
 ELF_SUFFIX=.elf
 
-if [[ $# != 2 ]]; then
-    echo "usage: $0.sh <prog> <server ip>"
+if [[ $# != 4 ]]; then
+    echo "usage: $0.sh <nocheri | purecap> <prog> <path_to_output_dir> <server ip>"
     exit 1
 fi
 
-PROG=$1
-SERVER_IP=$2
+CHERI=$1
+PROG=$2
+CHERI_DIR=$3
+SERVER_IP=$4
+
+# Identify the directory where cheribuild installs binaries
+if [[ ${CHERI} == "nocheri" ]]; then
+    BIN_DIR=${CHERI_DIR}/output/sdk/baremetal/baremetal-riscv64/riscv64-unknown-elf/FreeRTOS/Demo/bin
+elif [[ ${CHERI} == "purecap" ]]; then
+    BIN_DIR=${CHERI_DIR}/output/sdk/baremetal/baremetal-riscv64-purecap/riscv64-unknown-elf/FreeRTOS/Demo/bin
+else
+    echo "usage: $0.sh <nocheri | purecap> <prog> <path_to_output_dir> <server ip>"
+    exit 1
+fi
 
 scp ${BIN_DIR}/${PROG}*${ELF_SUFFIX} ${USERNAME}@${SERVER_IP}:~
